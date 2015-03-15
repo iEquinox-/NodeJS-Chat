c0a888f1b9feecb3d4ccee37fee46ed4f6c7249a
Grayson Equinox <duubz@skid.pro>
2015-03-15 05:17:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-var fs = require('fs');
-var http = require('http').createServer(function (req,res){
-	fs.readFile(__dirname+'/index.html', function(err,data) {
-		res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length':data.length});
-		res.write(data);
-		res.end();
-	});	
-}).listen(3000,'127.0.0.1');
-var io = require('socket.io')(http);
-
-var usernames = [],
-		users = 0,
-		open  = {};
+var fs = require('fs'),
+	ps = require('child_process'),
+	http = require('http').createServer(function (req,res){
+		fs.readFile(__dirname+'/index.html', function(err,data) {
+			res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length':data.length});
+			res.write(data);
+			res.end();
+		});	
+	}).listen(3000,'127.0.0.1');
+var io = require('socket.io')(http),
+	usernames = [],
+	users = 0,
+	open  = {};
+	
+var Settings = {
+		Botsettings: {
+			Botuser: "Intellect",
+			Botstyle: "color:#EC2F9A;"
+		}
+	}
 
 io.on('connection', function(socket){
 	users += 1;
+	open[socket.id] = socket;
 	io.emit('recountUsers', users);
 	socket.on('_Message', function(data) {
 		io.emit('_Message', data);
@@ -24,9 +32,14 @@ io.on('connection', function(socket){
 			open[socket.id].emit('_Return', false);
 		} else {
 			usernames.push(username);
-			sidi[socket.id] = username;
 			open[socket.id].emit('_Return', true);
 		}
+	});
+	
+	socket.on('_Command', function(data){
+		ps.spawn('python', [__dirname+'/bot.py', data]).stdout.on('data', function(rdata) {
+			open[socket.id].emit('_CMDReturn', {BOTname: "<span style=\""+Settings.Botsettings.Botstyle+"\">"+Settings.Botsettings.Botuser+"</span>", Data: rdata.toString('utf8')});
+		});
 	});
 	
 	socket.on('disconnect', function(){
